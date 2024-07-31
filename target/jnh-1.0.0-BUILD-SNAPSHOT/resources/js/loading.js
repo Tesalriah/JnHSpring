28f2818eef0540f689e1740b28f0d040c0ef1a6c
Tesalriah <95607242+Tesalriah@users.noreply.github.com>
2024-07-31 18:43:31 +0900

    Delete target directory

--- target/jnh-1.0.0-BUILD-SNAPSHOT/resources/js/loading.js
+++
@@ -1,7 +0,0 @@
-let submitBtn = document.querySelector("#submit");
-let loginFix = document.querySelector(".loding_fix")
-
-submitBtn.addEventListener("click", function (){
-    submitBtn.style.pointerEvents = "none";
-    loginFix.style.display = 'block';
-})
